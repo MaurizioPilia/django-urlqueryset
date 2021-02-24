a6bdb43bc91515401fed9d9ed1bdc1ec8c0ff2f8
MaurizioPilia <piliamaurizio@gmail.com>
2021-02-24 09:49:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_urlqueryset',
-    version='0.4.2',
+    version='0.4.3',
     packages=find_packages(),
     include_package_data=True,
     url='https://github.com/MaurizioPilia/django-urlqueryset',
